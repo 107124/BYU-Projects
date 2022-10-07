f334a95dfd8676bfba41dd960b03265336b8802b
107124 <kredfox4@gmail.com>
2022-10-07 11:01:11 -0400

    added fav_letter.py team activity

---
+++ for_loops_one.py
@@ -0,0 +1,11 @@
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color.title())
+
+for number in range(1, 9):
+    print(number)
+
+for number in range(1, 21):
+    if number % 2 == 0:
+        print(number)
