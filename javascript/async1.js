ca9ff825b5911d673b53da2079d3634c18b5c775
107124 <kredfox4@gmail.com>
2022-10-11 11:12:28 -0400

    added leason 9 shopping cart part 1 activity

---
+++ javascript/async1.js
@@ -0,0 +1,9 @@
+// 0a7120df71572a9c6659e5c412703074 api key for weatherweathermap.org
+
+console.log("hi")
+
+setTimeout(funciton() {
+    console.log("asynchronous result")
+}, 5000)
+
+console.log("synchronous result") 
