d37dbb109d8237938bf7e837efb72266ae65b71f
107124 <kredfox4@gmail.com>
2022-10-21 20:54:43 -0400

    finished the world assignment ready for submission

--- python_1/ourWorld/our_world.py
+++ python_1/ourWorld/our_world.py
@@ -1,36 +1,58 @@
+from xml.etree.ElementTree import QName
+
+
 year_list = []
 
 group_list = []
 av_life_list = []
 
+min = 200
+max = -1
+min_av = ""
+max_av = ""
+life_year_list = []
+
+new_max = -1
+new_min = 200
+search_max = ""
+search_min = ""
+
+
 with open("life-expectancy.csv") as facts:
-    # search = int(input("Enter a year of interest: "))
+    search = int(input("Enter a year of interest: "))
     for fact in facts:
-        line = fact.split("\n")
-        line = line[0].split(",")
-        country, country_code, year, life_ex = line
-        # year_list.append(year)
-        country = line[0]
-        country_code = line[1]
-        year = line[2]
-        life_ex = line[3]
-        group = (country, country_code, year, life_ex)
-        group_list.append(group)
-        year_list.append(year)
-        print(line)
+        try:
+            line = fact.strip()
+            line = line.split(",")
 
-    for pair in group_list:
-        av_life = pair[3]
-        av_life_list.append(av_life)
-        av_life_list.sort()
+            if float(line[3]) < min:
+                min = float(line[3])
+                min_av = line
 
-    biggest_av = av_life_list[-2]
-    smallest_av = av_life_list[0]
+            elif float(line[3]) > max:
+                max = float(line[3])
+                max_av = line
 
-    print(f"Smallest Average: {smallest_av}")
-    print(f"Biggest Average: {biggest_av}")
-    # print(year_list)
-    # print(av_life_list)
+            if int(line[2]) == search:
+                life_year_list.append(float(line[3]))
 
-    # pair = zip(year_list, av_life_list[0:-1])
-    # print(list(pair))
+                if float(line[3]) < new_min:
+                    new_min = float(line[3])
+                    search_min = line
+
+                elif float(line[3]) > new_max:
+                    new_max = float(line[3])
+                    search_max = line
+
+        except:
+            pass
+
+
+    print(f"\nOverall:\nmin: ", min)
+    print(f"Min line is: ", min_av)
+    print(f"\nmax: ", max)
+    print(f"max line is: ", max_av)
+    average = sum(life_year_list) / len(life_year_list)
+    print(f"\n\nThe average world life expectancy for the year {search} is : {average}")
+    print(f"\n{search_min[0].title()} has the lowest life expectancy average of {new_min} for the year {search}")
+    print(f"\n{search_max[0].title()} has the highest life expectancy average of {new_max} for the year {search}\n")
